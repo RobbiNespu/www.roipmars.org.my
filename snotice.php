47fbd8de2b2eabbce180e74a88827e6d3c059bd5
Hafizi Ruslan <pizi@hotmail.my>
2022-12-17 19:57:00 +0800

    add tawk chat

--- snotice.php
+++ snotice.php
@@ -290,6 +290,19 @@
       darkmode.toggleDarkMode();
     }
   </script>
+  <!--Start of Tawk.to Script-->
+  <script type='text/javascript'>
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+      var s1=document.createElement('script'), s0=document.getElementsByTagName('script')[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/639da9ffb0d6371309d4e1b5/1gkfu04hu';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+    })();
+  </script>
+  <!--End of Tawk.to Script-->
 </body>
 
 </html>

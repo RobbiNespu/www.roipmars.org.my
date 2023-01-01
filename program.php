aff2452fea7fe998f3f9bfc2503d2d0162c1227e
Hafizi Ruslan <pizi@hotmail.my>
2023-01-01 18:15:12 +0800

    replace rhn with sft on federal

--- program.php
+++ program.php
@@ -226,13 +226,13 @@
           <table class='table table-sm table-bordered table-striped text-center align-middle'>
             <thead><th>Minggu</th><th>Hari</th><th>Tarikh</th><th>Masa</th><th>Acara</th><th>Pengawal</th><th>Keputusan</th></thead>
             <tbody>
-              <tr bgcolor='#56C3E7'><td>5</td><td>Rabu</td><td>01 Feb 2023</td><td>9:00PM</td><td>&#91;SPECIAL&#93; English Net<br>&#40;Federal Territory Day&#41;</td><td>9W6RHN</td><td></td></tr>
+              <tr bgcolor='#56C3E7'><td>5</td><td>Rabu</td><td>01 Feb 2023</td><td>9:00PM</td><td>&#91;SPECIAL&#93; English Net<br>&#40;Federal Territory Day&#41;</td><td>9W2SFT</td><td></td></tr>
               <tr><td>5</td><td>Jumaat</td><td>03 Feb 2023</td><td>9:00PM</td><td>MARS Net</td><td>9W6AFF</td><td></td></tr>
               <tr bgcolor='#336699'><td>5</td><td>Sabtu</td><td>04 Feb 2023</td><td>9:00PM</td><td>&#91;KHAS&#93; MARS Net<br>&#40;Hari Kanser Sedunia&#41;</td><td>9W2SNN</td><td></td></tr>
               <tr bgcolor='#D71110'><td>5</td><td>Ahad</td><td>05 Feb 2023</td><td>9:00PM</td><td>&#91;KHAS&#93; CB Net<br>&#40;Hari Thaipusam&#41;</td><td>113DAISS</td><td></td></tr>
               <tr><td>6</td><td>Rabu</td><td>08 Feb 2023</td><td>9:00PM</td><td>English Net</td><td>9W8VWW</td><td></td></tr>
               <tr><td>6</td><td>Jumaat</td><td>10 Feb 2023</td><td>9:00PM</td><td>MARS Net</td><td>9W6PKJ</td><td></td></tr>
-              <tr><td>6</td><td>Sabtu</td><td>11 Feb 2023</td><td>9:00PM</td><td>MARS Net</td><td>9W2SFT</td><td></td></tr>
+              <tr><td>6</td><td>Sabtu</td><td>11 Feb 2023</td><td>9:00PM</td><td>MARS Net</td><td>9W6RHN</td><td></td></tr>
               <tr><td>6</td><td>Ahad</td><td>12 Feb 2023</td><td>9:00PM</td><td>CB Net</td><td>113DAJCP</td><td></td></tr>
               <tr><td>7</td><td>Rabu</td><td>15 Feb 2023</td><td>9:00PM</td><td>English Net</td><td>9W2EZK</td><td></td></tr>
               <tr><td>7</td><td>Jumaat</td><td>17 Feb 2023</td><td>9:00PM</td><td>MARS Net</td><td>9W2YJT</td><td></td></tr>

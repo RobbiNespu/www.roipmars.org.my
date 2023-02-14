d2b57c768157735c29642fb9d3be75fc4eecab01
Hafizi Ruslan <pizi@hotmail.my>
2023-02-15 01:03:57 +0800

    left q1 to display

--- program.php
+++ program.php
@@ -428,6 +428,7 @@
         </div>
       </div>
     </div>
+<!--
     <div class='row'>
       <div class='col-lg-4'>
         <div class='d-grid gap-1 my-1'>
@@ -918,6 +919,7 @@
         </div>
       </div>
     </div>
+-->
     <hr>
     <div class='row'>
       <div class='col-12 my-2 d-block mx-auto'>

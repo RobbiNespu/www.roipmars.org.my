aa8ce1794b69267771430439e042aecabcf67958
Hafizi Ruslan <pizi@hotmail.my>
2023-02-19 03:12:09 +0800

    update img url

--- howto.php
+++ howto.php
@@ -17,16 +17,16 @@
   <meta property='og:title' content='RoIPMARS | Tutorial'>
   <meta property='og:description' content='Persatuan Peminat Radio Komunikasi (RoIP) Tutorial Sambungan'>
   <meta property='og:url' content='https://www.roipmars.org.my/tutorial'>
-  <meta property='og:image' content='https://www.roipmars.org.my/media/image/brand_sq.png'>
+  <meta property='og:image' content='media/image/brand_sq.png'>
   <meta property='og:type' content='website'>
   <meta property='og:locale' content='ms-MY'>
   <meta name='twitter:title' content='RoIPMARS | Tutorial'>
   <meta name='twitter:description' content='Persatuan Peminat Radio Komunikasi (RoIP) Tutorial Sambungan'>
   <meta name='twitter:url' content='https://www.roipmars.org.my/tutorial'>
-  <meta name='twitter:image' content='https://www.roipmars.org.my/media/image/brand_sq.png'>
+  <meta name='twitter:image' content='media/image/brand_sq.png'>
   <meta name='twitter:creator' content='@HafiziRuslan'>
   <meta name='msapplication-TileColor' content='#56C3E7'>
-  <meta name='msapplication-TileImage' content='./images/ms-icon-144x144.png'>
+  <meta name='msapplication-TileImage' content='media/image/ms-icon-144x144.png'>
   <meta name='theme-color' media='(prefers-color-scheme: light)' content='#336699'>
   <meta name='theme-color' media='(prefers-color-scheme: dark)' content='#56C3E7'>
   <link rel='shortcut icon' type='image/x-icon' href='media/image/favicon.ico'>
--- nts/2020/03.php
+++ nts/2020/03.php
@@ -17,16 +17,16 @@
   <meta property='og:title' content='RoIPMARS | Downloads Webpage'>
   <meta property='og:description' content='Persatuan Peminat Radio Komunikasi (RoIP) Downloads Webpage'>
   <meta property='og:url' content='https://www.roipmars.org.my/nts/2020/03'>
-  <meta property='og:image' content='https://www.roipmars.org.my/media/image/brand_sq.png'>
+  <meta property='og:image' content='media/image/brand_sq.png'>
   <meta property='og:type' content='website'>
   <meta property='og:locale' content='ms-MY'>
   <meta name='twitter:title' content='RoIPMARS | Downloads Webpage'>
   <meta name='twitter:description' content='Persatuan Peminat Radio Komunikasi (RoIP) Downloads Webpage'>
   <meta name='twitter:url' content='https://www.roipmars.org.my/nts/2020/03'>
-  <meta name='twitter:image' content='https://www.roipmars.org.my/media/image/brand_sq.png'>
+  <meta name='twitter:image' content='media/image/brand_sq.png'>
   <meta name='twitter:creator' content='@HafiziRuslan'>
   <meta name='msapplication-TileColor' content='#56C3E7'>
-  <meta name='msapplication-TileImage' content='../../images/ms-icon-144x144.png'>
+  <meta name='msapplication-TileImage' content='../.media/image/ms-icon-144x144.png'>
   <meta name='theme-color' media='(prefers-color-scheme: light)' content='#336699'>
   <meta name='theme-color' media='(prefers-color-scheme: dark)' content='#56C3E7'>
   <meta name='color-scheme' content='dark'>

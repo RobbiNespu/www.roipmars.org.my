311c2318e9004b0829bef973cee21ca574ad25fe
Hafizi Ruslan <pizi@hotmail.my>
2023-02-19 03:23:51 +0800

    staging 190223

---
+++ rnd.php
@@ -0,0 +1,191 @@
+<!doctype html>
+<html lang='ms-MY' data-bs-theme='dark'>
+
+<head>
+  <title>RoIPMARS | Penyelidikan &amp; Pembangunan</title>
+  <meta charset='utf-8'>
+  <meta http-equiv='Content-Type' content='text/html'>
+  <meta http-equiv='content-language' content='ms-MY'>
+  <meta name='language' content='Malay'>
+  <meta name='author' content='HafiziRuslan'>
+  <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+  <meta name='title' content='RoIPMARS | Penyelidikan & Pembangunan'>
+  <meta name='description' content='Persatuan Peminat Radio Komunikasi (RoIP) Penyelidikan & Pembangunan'>
+  <meta name='keywords' content='roipmars, malaysiats3, ts3servermalaysia, teamspeak3malaysia, persatuanpeminatradiokomunikasi, roipmalaysia, cbroipmalaysia, amaturroipmalaysia'>
+  <meta name='robots' content='index, follow'>
+  <meta property='og:site_name' content='RoIPMARS'>
+  <meta property='og:title' content='RoIPMARS | Penyelidikan & Pembangunan'>
+  <meta property='og:description' content='Persatuan Peminat Radio Komunikasi (RoIP) Penyelidikan & Pembangunan'>
+  <meta property='og:url' content='https://www.roipmars.org.my/rnd'>
+  <meta property='og:image' content='media/image/brand_sq.png'>
+  <meta property='og:type' content='website'>
+  <meta property='og:locale' content='ms-MY'>
+  <meta name='twitter:title' content='RoIPMARS | Penyelidikan & Pembangunan'>
+  <meta name='twitter:description' content='Persatuan Peminat Radio Komunikasi (RoIP) Penyelidikan & Pembangunan'>
+  <meta name='twitter:url' content='https://www.roipmars.org.my/rnd'>
+  <meta name='twitter:image' content='media/image/brand_sq.png'>
+  <meta name='twitter:creator' content='@HafiziRuslan'>
+  <meta name='msapplication-TileColor' content='#56C3E7'>
+  <meta name='msapplication-TileImage' content='media/image/ms-icon-144x144.png'>
+  <meta name='theme-color' media='(prefers-color-scheme: light)' content='#336699'>
+  <meta name='theme-color' media='(prefers-color-scheme: dark)' content='#56C3E7'>
+  <link rel='shortcut icon' type='image/x-icon' href='media/image/favicon.ico'>
+  <link rel='icon' type='image/x-icon' href='media/image/favicon.ico'>
+  <link rel='icon' type='image/png' sizes='16x16' href='media/image/favicon-16x16.png'>
+  <link rel='icon' type='image/png' sizes='32x32' href='media/image/favicon-32x32.png'>
+  <link rel='icon' type='image/png' sizes='96x96' href='media/image/favicon-96x96.png'>
+  <link rel='icon' type='image/png' sizes='192x192' href='media/image/android-icon-192x192.png'>
+  <link rel='apple-touch-icon' sizes='57x57' href='media/image/apple-icon-57x57.png'>
+  <link rel='apple-touch-icon' sizes='60x60' href='media/image/apple-icon-60x60.png'>
+  <link rel='apple-touch-icon' sizes='72x72' href='media/image/apple-icon-72x72.png'>
+  <link rel='apple-touch-icon' sizes='76x76' href='media/image/apple-icon-76x76.png'>
+  <link rel='apple-touch-icon' sizes='114x114' href='media/image/apple-icon-114x114.png'>
+  <link rel='apple-touch-icon' sizes='120x120' href='media/image/apple-icon-120x120.png'>
+  <link rel='apple-touch-icon' sizes='144x144' href='media/image/apple-icon-144x144.png'>
+  <link rel='apple-touch-icon' sizes='152x152' href='media/image/apple-icon-152x152.png'>
+  <link rel='apple-touch-icon' sizes='180x180' href='media/image/apple-icon-180x180.png'>
+  <link rel='manifest' href='./vendor/manifest.json'>
+  <link rel='stylesheet' href='https://use.typekit.net/kew7gwq.css'>
+  <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css' crossorigin='anonymous'>
+  <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css' crossorigin='anonymous'>
+  <script src='https://kit.fontawesome.com/221737b641.js' crossorigin='anonymous'></script>
+  <script src='./vendor/js/gtm.js'></script>
+  <script async src='https://www.googletagmanager.com/gtag/js?id=G-DJYTZHZXFN'></script>
+  <script src='./vendor/js/gtag.js'></script>
+  <script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1205248207690495' crossorigin='anonymous'></script>
+  <script async src='https://fundingchoicesmessages.google.com/i/pub-1205248207690495?ers=1' nonce='nLBiTr-Hd6zWbl5ZT9wqvA'></script>
+  <script nonce='nLBiTr-Hd6zWbl5ZT9wqvA' src='./vendor/js/gfc.js'></script>
+  <script src='./vendor/js/clarity.js'></script>
+</head>
+
+<body>
+  <noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-PHLPNS8' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>
+  <div id='fb-root'></div>
+  <div id='fb-customer-chat' class='fb-customerchat'></div>
+  <script src='./vendor/js/fbchat.js'></script>
+  <nav class='navbar sticky-top navbar-expand-xl bg-body-tertiary'>
+    <div class='container'>
+      <a class='navbar-brand' href='https://www.roipmars.org.my'><img class='img-fluid d-inline-block' src='media/image/brand.png' width='150' alt='PERSATUAN PEMINAT RADIO KOMUNIKASI (ROIP)'></a>
+      <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'><span class='navbar-toggler-icon'></span></button>
+      <div class='collapse navbar-collapse' id='navbarNav'>
+        <ul class='nav navbar-nav nav-pills fs-5 text-xl-center text-end d-flex mx-auto'>
+          <li class='nav-item'><a class='nav-link' href='about'>Tentang Kami</a></li>
+          <li class='nav-item'><a class='nav-link' href='activities'>Aktiviti</a></li>
+          <li class='nav-item'><a class='nav-link' href='program'>Program</a></li>
+          <li class='nav-item'><a class='nav-link active rounded-3' aria-current='page' href='rnd'>Penyelidikan</a></li>
+          <li class='nav-item'><a class='nav-link' href='howto'>Tutorial</a></li>
+          <li class='nav-item'><a class='nav-link' href='cbinfo'>Informasi Jalur Rakyat</a></li>
+          <li class='nav-item'><a class='nav-link' href='cbmars'>CB RoIPMARS</a></li>
+          <li class='nav-item'><a class='nav-link' href='member'>Keahlian</a></li>
+          <li class='nav-item'><a class='nav-link' href='snotice'>Notis Perkhidmatan</a></li>
+          <!--<li class='nav-item'><a class='nav-link' href='mailus'>Hubungi Kami</a></li>-->
+          <li class='nav-item'><a class='nav-link' href='https://kedai.roipmars.org.my' target='_blank'><i class='bi-cart4'></i> Kedai</a></li>
+          <li class='nav-item'><a class='nav-link' href='https://ftp.roipmars.org.my' target='_blank'><i class='bi-download'></i> Muat Turun</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class='container-fluid'>
+    <div class='row text-center'>
+      <div class='col mt-2 mb-2 jumbotron-fluid'>
+        <h1 class='h1-responsive'>Penyelidikan &amp; Pembangunan</h1>
+      </div>
+    </div>
+  </div>
+  <div class='container'>
+    <div class='row my-3'>
+      <div class='col-12'>
+        <h2 class='h2 text-center'>Pengurusan Rangkaian Kumpulan RoIPMARS</h2>
+      </div>
+      <div class='col-lg-6 my-2'>
+        <div class='row g-1 text-start'>
+          <div class='col-2 align-self-center'>
+            <img src='media/image/member/002.png' class='img-fluid rounded-circle object-fit-fill bg-dark-subtle bg-gradient' alt='9W2UZL'>
+          </div>
+          <div class='col-10'>
+            <div class='card-body mx-3'>
+              <p class='card-title fs-3 fw-bold'>9W2UZL &ndash; ZULKIFLI ABU</p>
+              <p class='card-subtitle fw-semibold'>FOUNDER, LEAD ADMINISTRATOR &amp; CHIEF RESEARCHER</p>
+              <p class='card-subtitle fw-bolder'>PENGASAS, KETUA PENTADBIR &amp; KETUA PENYELIDIK</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class='col-lg-6 my-2'>
+        <div class='row g-1 text-end'>
+          <div class='col-10'>
+            <div class='card-body mx-3'>
+              <p class='card-title fs-3 fw-bold'>9W2LGX &ndash; HAFIZI RUSLAN</p>
+              <p class='card-subtitle fw-semibold'>SOFTWARE DEVELOPER &amp; LEAD NETWORK ADMINISTRATOR</p>
+              <p class='card-subtitle fw-bolder'>PEMBANGUN PERISIAN &amp; KETUA PENTADBIR RANGKAIAN</p>
+            </div>
+          </div>
+          <div class='col-2 align-self-center'>
+            <img src='media/image/member/001.png' class='img-fluid rounded-circle object-fit-fill bg-dark-subtle bg-gradient' alt='9W2LGX'>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer class='footer mt-auto py-1 page-footer font-small bg-body-tertiary text-center'>
+    <div class='container'>
+      <div class='row my-0'>
+        <div class='col-lg-8 mx-auto my-1'>
+          <h5 class='text-uppercase mb-2 fw-bold text-md-start'>Sumbangan</h5>
+          <p class='mb-2 text-md-start'>Sumbangan &amp; Derma anda membantu Penyelidikan &amp; Pembangunan kami serta semua Perkhidmatan kami tersedia untuk anda pada bila-bila masa di mana sahaja</p>
+          <table class='table table-sm table-bordered table-striped text-center align-middle text-wrap my-0 mx-auto w-auto' style='white-space: nowrap'>
+            <tbody>
+              <tr><th>Nama</th><td>PERSATUAN PEMINAT RADIO KOMUNIKASI &#40;ROIP&#41;</td></tr>
+              <tr><th>Bank</th><td>BANK MUAMALAT MALAYSIA BERHAD / BMMBMYKL</td></tr>
+              <tr><th>Akaun</th><td>1214 0000 4377 12</td></tr>
+            </tbody>
+          </table>
+          <p class='my-0'>Kami juga menerima <a class='text-decoration-none' href='#' onClick='DonateWindow=window.open("https://payment.tngdigital.com.my/sc/bDLnBmDoDq", "TNGD RoIPMARS Donation", "width=800,height=600"); return false;'><img src='media/image/tngew_ht.png' class='img-fluid' width='50px'></a> <a class='text-decoration-none' href='#' onClick='DonateWindow=window.open("https://toyyibpay.com/sumbanganroipmars", "FPX Donation", "width=800,height=600"); return false;'><img src='media/image/fpx.png' class='img-fluid bg-light-subtle' width='50px'></a> <a class='text-decoration-none' href='#' onClick='DonateWindow=window.open("https://donate.stripe.com/5kA8AceIg7Gegbm5kk", "Stripe Donation", "width=800,height=600"); return false;'><i class='bi-credit-card fs-4 align-middle'></i></a></p>
+          <p class='my-0'>&mdash; DARI ANDA KEMBALI KEPADA ANDA &mdash;</p>
+        </div>
+        <div class='col-lg-4 mx-auto my-1'>
+          <h5 class='text-uppercase mb-2 fw-bold text-md-end'>Hubungi Kami</h5>
+          <div class='d-grid gap-2 d-md-flex justify-content-md-end mb-2'>
+            <a type='button' class='btn btn-primary rounded-5 me-md-2' href='mailto:member@roipmars.org.my?subject=Email%20Sent%20from%20Website'><i class='bi-envelope-at'></i> member@roipmars.org.my</a>
+            <button type='button' class='btn btn-primary rounded-5' data-bs-toggle='modal' data-bs-target='.bd-subnewsletter-modal-xl'><i class='bi-envelope-paper'></i> Langgan surat berita kami</button>
+          </div>
+            <div class='modal fade bd-subnewsletter-modal-xl' tabindex='-1' role='dialog' aria-labelledby='subnewsletterModalLabel' aria-hidden='true'>
+              <div class='modal-dialog modal-dialog-centered' role='document'>
+                <div class='modal-content'>
+                  <div class='modal-body'>
+                    <div class='ratio ratio-1x1'>
+                      <iframe src='https://1306b3f7.sibforms.com/serve/MUIEAIR2UxOW8JezeVtN05wHT8tqVrcROKOSDLHHB5BNy1LHST-CbEPqTvRhQe5_CrrvOOmbedVNkoHAJxlEwQ8tbScw_7eHtjByIUNLUHOsQBo7rmMribZ9ZpPLC89V-9lSyMybFpSBE8ClRHD6itpcxnb_bpZk-06g7sjz4NaQn2gIAlL_0HNtA_BA7UvZT5jIptMIZetcXcSB'></iframe>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          <div class='d-md-flex justify-content-md-end'>
+            <img class='img-fluid rounded-5 bg-secondary me-md-2' loading='lazy' src='https://www.hamqsl.com/solar101sc.php?bgcolor=white&back=transparent&heline=eve'>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class='container'>
+      <div class='row my-2 d-flex align-middle'>
+        <div class='col-md-8 col-lg-8 col-xl-8'>
+          <p class='text-lg-start font-monospace'><a href='policies' class='text-reset text-decoration-none'>Polisi Perkhidmatan</a><span class='text-muted'> &#124; <i class='fa-brands fa-php'></i> <i class='fa-brands fa-html5'></i> <i class='fa-brands fa-bootstrap'></i> <i class='fa-brands fa-css3'></i> <i class='fa-brands fa-font-awesome'></i> <i class='fa-brands fa-java'></i> <i class='fa-brands fa-cloudflare'></i></span><br>&copy;2014&mdash;<script>document.write(new Date().getFullYear())</script> PERSATUAN PEMINAT RADIO KOMUNIKASI &#40;ROIP &#91;RADIO OVER INTERNET PROTOCOL&#93;&#41; PPM-006-10-01062020</p>
+        </div> 
+        <div class='col-md-4 col-lg-4 col-xl-4 ml-lg-0'>
+          <div class='text-lg-end'>
+            <a href='https://fb.com/roipmars.org.my'><i class='bi-facebook h1 mx-1'></i></a>
+            <a href='https://t.me/marsts3servercyberjaya'><i class='bi-telegram h1 mx-1'></i></a>
+            <a href='https://wa.me/c/60333960874'><i class='bi-whatsapp h1 mx-1'></i></a>
+            <a href='#' onClick='TSLoginWindow=window.open("https://www.roipmars.org.my/applogin", "Login RoIPMARS Comm Apps", "width=800,height=600"); return false;'><i class='bi-headset h1 mx-1'></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <script src='https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js' crossorigin='anonymous'></script>
+  <script src='https://cdn.jsdelivr.net/npm/@popperjs/core@2/dist/umd/popper.min.js' crossorigin='anonymous'></script>
+  <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js' crossorigin='anonymous'></script></script>
+  <script src='./vendor/js/tawkto.js'></script>
+</body>
+
+</html>

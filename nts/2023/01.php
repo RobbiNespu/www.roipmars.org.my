39287b8ab2a8356bc1d0238d58cad6cd66cd017d
Hafizi Ruslan <pizi@hotmail.my>
2023-02-25 17:59:41 +0800

    redirect https

--- nts/2023/01.php
+++ nts/2023/01.php
@@ -72,7 +72,7 @@
     <p class='lh-sm'>Pengguna yang dihormati,</p>
     <p class='fs-2 fw-bold text-decoration-underline' style='font-family: "Nanum Gothic Coding", monospace'>Per: Sambungan Pengeluaran Sijil NCS &amp; Kad QSL</p>
     <p class='mb-2'>Merujuk kepada notis &#40;<a href='../2020/02'>NtsMARS2020/02</a>&#41;, ingin dimaklumkan bahawa kami akan menyambung semula pengeluaran sijil dan kad QSL pada setiap acara yang telah dirancang sepanjang tahun 2023.</p>
-    <p>Segala sijil dan kad QSL anda akan dikemaskini dari masa ke semasa dan boleh didapati di laman sesawang <a href='http://ftp.roipmars.org.my'>ftp.roipmars.org.my</a>.</p>
+    <p>Segala sijil dan kad QSL anda akan dikemaskini dari masa ke semasa dan boleh didapati di laman sesawang <a href='https://ftp.roipmars.org.my'>ftp.roipmars.org.my</a>.</p>
     <p>Diharap pihak tuan dapat memaklumkan perkara ini kepada rakan-rakan dan sahabat handai. Sokongan dan bantuan anda semua dapat membantu usaha kami untuk kekal aktif dan relevan sepanjang masa.</p>
     <p>Jutaan terima kasih kami ucapkan atas segala sokongan anda.</p>
     <p class='text-center'>Sekiranya anda memerlukan bantuan, sila email kepada: <a href='mailto:member@roipmars.org.my?subject=Inkuiri%20Notis2301'>member@roipmars.org.my</a></p>

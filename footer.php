c557f310c5022c1abd91d1b4460ae2a5e47d88cf
Hafizi Ruslan <pizi@hotmail.my>
2023-03-28 07:22:06 +0800

    simplify cbinfo table

--- footer.php
+++ footer.php
@@ -16,18 +16,9 @@
           <p class='mb-2'>Sumbangan &amp; Derma anda membantu Penyelidikan &amp; Pembangunan serta Perkhidmatan kami tersedia untuk anda sepanjang masa di mana sahaja.</p>
           <table class='table table-sm table-bordered text-center align-middle text-wrap my-0 mx-auto w-auto' style='white-space: nowrap'>
             <tbody>
-              <tr>
-                <th>Nama</th>
-                <td>PERSATUAN PEMINAT RADIO KOMUNIKASI &#40;ROIP &#40;RADIO OVER INTERNET PROTOCOL&#41;&#41;</td>
-              </tr>
-              <tr>
-                <th>Bank</th>
-                <td>BANK MUAMALAT MALAYSIA BERHAD / BMMBMYKL</td>
-              </tr>
-              <tr>
-                <th>Akaun</th>
-                <td>1214 0000 4377 12</td>
-              </tr>
+              <tr><th>Nama</th><td>PERSATUAN PEMINAT RADIO KOMUNIKASI &#40;ROIP &#40;RADIO OVER INTERNET PROTOCOL&#41;&#41;</td></tr>
+              <tr><th>Bank</th><td>BANK MUAMALAT MALAYSIA BERHAD / BMMBMYKL</td></tr>
+              <tr><th>Akaun</th><td>1214 0000 4377 12</td></tr>
             </tbody>
           </table>
           <p class='mt-2 mb-0 text-center'>Kami juga menerima <a class='text-reset text-decoration-none' href='#' onClick='donateTngd()'><img src='media/image/tngew_ht.png' class='img-fluid bg-light rounded-2 px-1' width='45'></a> | <a class='text-reset text-decoration-none' href='#' onClick='donateToyyib()'><img src='media/image/fpx.png' class='img-fluid bg-light rounded-2 p-1' width='50'></a> | <a class='text-reset text-decoration-none' href='#' onClick='donateStripe()'><i class='bi-credit-card text-white fs-2 align-middle'></i></a></p>
@@ -37,8 +28,7 @@
           <h5 class='text-uppercase mb-2 fw-bold text-md-end'>Hubungi Kami</h5>
           <div class='d-grid gap-1 d-md-flex justify-content-md-end mb-2'>
             <a type='button' class='btn btn-outline-info rounded-5 me-md-2' href='mailto:member@roipmars.org.my?subject=Email%20Sent%20from%20Website'><i class='bi-envelope-at'></i> member@roipmars.org.my</a>
-            <button type='button' class='btn btn-outline-info rounded-5' data-bs-toggle='modal' data-bs-target='.bd-subnewsletter-modal-xl'><i class='bi-envelope-paper'></i> Langgan surat berita
-              kami</button>
+            <button type='button' class='btn btn-outline-info rounded-5' data-bs-toggle='modal' data-bs-target='.bd-subnewsletter-modal-xl'><i class='bi-envelope-paper'></i> Langgan surat berita              kami</button>
           </div>
           <div class='modal fade bd-subnewsletter-modal-xl' tabindex='-1' role='dialog' aria-labelledby='subnewsletterModalLabel' aria-hidden='true'>
             <div class='modal-dialog modal-dialog-centered' role='document'>

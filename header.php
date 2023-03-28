f3d3d4826fc3843e7396d1b0e6723431e48ca848
Hafizi Ruslan <pizi@hotmail.my>
2023-03-29 05:45:44 +0800

    format file

--- header.php
+++ header.php
@@ -1,56 +1,56 @@
 <!doctype html>
 <html data-bs-theme='dark'>
 
-<head>
-  <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.css' crossorigin='anonymous'>
-  <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css' crossorigin='anonymous'>
-  <script src='https://kit.fontawesome.com/221737b641.js' crossorigin='anonymous'></script>
-</head>
+  <head>
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.css' crossorigin='anonymous'>
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css' crossorigin='anonymous'>
+    <script src='https://kit.fontawesome.com/221737b641.js' crossorigin='anonymous'></script>
+  </head>
 
-<body>
-  <header>
-    <nav class='navbar navbar-expand-xl bg-body-tertiary'>
-      <div class='container'>
-        <a class='navbar-brand' href='https://www.roipmars.org.my'><img class='img-fluid d-inline-block' src='media/image/brand.png' width='150' alt='PERSATUAN PEMINAT RADIO KOMUNIKASI (ROIP)'></a>
-        <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='navbar'><span class='navbar-toggler-icon'></span></button>
-        <div class='collapse navbar-collapse' id='navbarNav'>
-          <ul class='nav navbar-nav nav-pills text-center d-flex ms-auto'>
-            <li class='nav-item'><a class='nav-link' href='about'>Tentang Kami</a></li>
-            <li class='nav-item'><a class='nav-link' href='activity'>Galeri</a></li>
-            <li class='nav-item'><a class='nav-link' href='rnd'>Kajian</a></li>
-            <li class='nav-item dropdown'>
-              <a class='nav-link dropdown-toggle' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'>Jalur Rakyat</a>
-              <ul class='dropdown-menu text-center'>
-                <li><a class='dropdown-item' href='cbinfo'>Informasi Jalur Rakyat</a></li>
-                <li><a class='dropdown-item' href='cbmars'>CB RoIPMARS</a></li>
-                <li>
-                  <hr class='dropdown-divider'>
-                </li>
-                <li><a class='dropdown-item' href='https://www.roipdarulaman.com'>CB RoIPDarulAman</a></li>
-              </ul>
-            </li>
-            <li class='nav-item dropdown'>
-              <a class='nav-link dropdown-toggle' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'>Kumpulan RoIPMARS</a>
-              <ul class='dropdown-menu text-center'>
-                <li><a class='dropdown-item' href='howto'>Tutorial</a></li>
-                <li><a class='dropdown-item' href='program'>Program RoIPMARS</a></li>
-                <li><a class='dropdown-item' href='snotice'>Notis Perkhidmatan</a></li>
-                <li><a class='dropdown-item' href='faq'>Soalan Lazim</a></li>
-                <li><a class='dropdown-item' href='member'>Keahlian RoIPMARS</a></li>
-                <!--<li><a class='dropdown-item' href='mailus'>Hubungi Kami</a></li>-->
-              </ul>
-            </li>
-            <li class='nav-item'><a class='nav-link' href='https://books.zoho.com/portal/roipmarsorgmy/' target='_blank'><i class='bi-person-vcard'></i> Portal Ahli</a></li>
-            <li class='nav-item'><a class='nav-link' href='https://kedai.roipmars.org.my' target='_blank'><i class='bi-cart4'></i> Kedai</a></li>
-            <li class='nav-item'><a class='nav-link' href='https://ftp.roipmars.org.my' target='_blank'><i class='bi-download'></i> Muat Turun</a></li>
-          </ul>
+  <body>
+    <header>
+      <nav class='navbar navbar-expand-xl bg-body-tertiary'>
+        <div class='container'>
+          <a class='navbar-brand' href='https://www.roipmars.org.my'><img class='img-fluid d-inline-block' src='media/image/brand.png' width='150' alt='PERSATUAN PEMINAT RADIO KOMUNIKASI (ROIP)'></a>
+          <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='navbar'><span class='navbar-toggler-icon'></span></button>
+          <div class='collapse navbar-collapse' id='navbarNav'>
+            <ul class='nav navbar-nav nav-pills text-center d-flex ms-auto'>
+              <li class='nav-item'><a class='nav-link' href='about'>Tentang Kami</a></li>
+              <li class='nav-item'><a class='nav-link' href='activity'>Galeri</a></li>
+              <li class='nav-item'><a class='nav-link' href='rnd'>Kajian</a></li>
+              <li class='nav-item dropdown'>
+                <a class='nav-link dropdown-toggle' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'>Jalur Rakyat</a>
+                <ul class='dropdown-menu text-center'>
+                  <li><a class='dropdown-item' href='cbinfo'>Informasi Jalur Rakyat</a></li>
+                  <li><a class='dropdown-item' href='cbmars'>CB RoIPMARS</a></li>
+                  <li>
+                    <hr class='dropdown-divider'>
+                  </li>
+                  <li><a class='dropdown-item' href='https://www.roipdarulaman.com'>CB RoIPDarulAman</a></li>
+                </ul>
+              </li>
+              <li class='nav-item dropdown'>
+                <a class='nav-link dropdown-toggle' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'>Kumpulan RoIPMARS</a>
+                <ul class='dropdown-menu text-center'>
+                  <li><a class='dropdown-item' href='howto'>Tutorial</a></li>
+                  <li><a class='dropdown-item' href='program'>Program RoIPMARS</a></li>
+                  <li><a class='dropdown-item' href='snotice'>Notis Perkhidmatan</a></li>
+                  <li><a class='dropdown-item' href='faq'>Soalan Lazim</a></li>
+                  <li><a class='dropdown-item' href='member'>Keahlian RoIPMARS</a></li>
+                  <!--<li><a class='dropdown-item' href='mailus'>Hubungi Kami</a></li>-->
+                </ul>
+              </li>
+              <li class='nav-item'><a class='nav-link' href='https://books.zoho.com/portal/roipmarsorgmy/' target='_blank'><i class='bi-person-vcard'></i> Portal Ahli</a></li>
+              <li class='nav-item'><a class='nav-link' href='https://kedai.roipmars.org.my' target='_blank'><i class='bi-cart4'></i> Kedai</a></li>
+              <li class='nav-item'><a class='nav-link' href='https://ftp.roipmars.org.my' target='_blank'><i class='bi-download'></i> Muat Turun</a></li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  </header>
-  <script src='https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.js' crossorigin='anonymous'></script>
-	<script src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.js' crossorigin='anonymous'></script>
-  <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.js' crossorigin='anonymous'></script>
-</body>
+      </nav>
+    </header>
+    <script src='https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.js' crossorigin='anonymous'></script>
+    <script src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.js' crossorigin='anonymous'></script>
+    <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.js' crossorigin='anonymous'></script>
+  </body>
 
 </html>

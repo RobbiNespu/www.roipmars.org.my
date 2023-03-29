cd53f524eff8604ae6437c59d75df14c5a33c69d
Hafizi Ruslan <pizi@hotmail.my>
2023-03-30 03:41:09 +0800

    update statpage popper

--- footer.min.php
+++ footer.min.php
@@ -29,6 +29,7 @@
     <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.js' crossorigin='anonymous'></script>
     <script src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.js' crossorigin='anonymous'></script>
     <script src='https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.js' crossorigin='anonymous'></script>
+    <script src='https://roipmarsnetwork.statuspage.io/embed/script.js' crossorigin='anonymous'></script>
   </body>
 
 </html>
--- vendor/js/statpage.js
+++
@@ -1,8 +0,0 @@
-import 'https://cdn.statuspage.io/se-v2.js';
-var sp = new StatusPage.page({ page: 'l6phyl70dpbh' });
-
-var syssummary = fetch('https://l6phyl70dpbh.statuspage.io/api/v2/summary.json');
-var allsysstat = fetch('https://l6phyl70dpbh.statuspage.io/api/v2/status.json');
-var syscompstat = fetch('https://l6phyl70dpbh.statuspage.io/api/v2/components.json');
-
-document('#sysstat').innerhtml = allsysstat.status.description

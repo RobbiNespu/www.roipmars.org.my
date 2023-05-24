5ef8c6dfa648d488230e617724bbd78c34678a6e
Hafizi Ruslan <pizi@hotmail.my>
2023-05-24 23:44:25 +0800

    update jun schedule

--- program.php
+++ program.php
@@ -510,8 +510,7 @@
 							<tbody>
 								<tr class='bg-primary-subtle'><td>Khamis<br>01/06</td><td>2100MY</td><td>&#91;KHAS&#93; RoIPMARS Net<br>&#40;Hari Ulang Tahun RoIPMARS ke-3&#41;</td><td>9W2UZL</td><td>&shy;</td></tr>
 								<tr class='bg-primary-subtle'><td>Jumaat<br>02/06</td><td>2100MY</td><td>&#91;KHAS&#93; RoIPMARS Net Santai<br>&#40;Hari Gawai&#41;<br>&#40;Hari Tentera Udara Diraja Malaysia&#41;</td><td>9M2SFL</td><td>&shy;</td></tr>
-								<tr class='bg-dark-subtle'><td rowspan='2'>Sabtu<br>03/06</td><td>TBC</td><td>Sambutan Ulang Tahun RoIPMARS ke-3<td colspan='2'>TBC</td></tr>
-								<tr><td>2100MY</td><td>&#91;KHAS&#93; RoIPMARS Net Malam Minggu<br>&#40;Hari Basikal Sedunia&#41;</td><td>9M6RMD</td><td>&shy;</td></tr>
+								<tr class='bg-dark-subtle'><td>Sabtu<br>03/06</td><td>2100MY</td><td>&#91;KHAS&#93; RoIPMARS Net Malam Minggu<br>&#40;Hari Basikal Sedunia&#41;</td><td>9M6RMD</td><td>&shy;</td></tr>
 								<tr><td>Ahad<br>04/06</td><td>2100MY</td><td>CB Net Hujung Minggu</td><td>113DAJCP</td><td>&shy;</td></tr>
 								<tr class='bg-primary-subtle'><td>Isnin<br>05/06</td><td>2100MY</td><td>&#91;KHAS&#93; RoIPMARS Net Awal Minggu<br>&#40;Hari Alam Sekitar Sedunia&#41;</td><td>9W2AYP</td><td>&shy;</td></tr>
 								<tr><td>Rabu<br>07/06</td><td>2100MY</td><td>Learning English Net</td><td>9M6JAY</td><td>&shy;</td></tr>

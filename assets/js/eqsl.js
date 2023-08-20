b519ed946bff78dfb035d282ababf79a1a766694
Hafizi Ruslan <pizi@hotmail.my>
2023-08-21 01:36:59 +0800

    debug pdfMake

--- assets/js/eqsl.js
+++ assets/js/eqsl.js
@@ -1,23 +1,32 @@
 import PDFDocument from 'pdfkit'
 import blobStream from 'blob-stream'
 
-function eQSLasPDF(call,mode,time) {
+function downeQSL(call, mode, time) {  
   const doc = new PDFDocument()
   const stream = doc.pipe(blobStream())
-
-  doc.image('../../media/image/brands/roipmars/brand.png',{
-    fit: [100,500],
-    align: 'top',
-    valign: 'center'
-  })
-  doc.font('Calibri').fontSize(12).text(call)
-  doc.font('Calibri').fontSize(12).text(mode)
-  doc.font('Calibri').fontSize(12).text(time)
-
+    
+  // add your content to the document here, as usual
+  
+  doc.fontSize(25).text(call + ' ' + mode + ' ' + time, 100, 100)
   doc.end()
-  stream.on('finish',function() {
-    const blob = stream.toBlob('application/pdf')
-    const url = stream.toBlobURL('application/pdf')
-    iframe.src = url
+
+  const a = document.createElement("a")
+  document.body.appendChild(a)
+  a.style = "display: none"
+
+  let blob
+
+  function download() {
+    if (!blob) return
+    var url = window.URL.createObjectURL(blob)
+    a.href = url
+    a.download = 'test.pdf'
+    a.click()
+    window.URL.revokeObjectURL(url)
+  }
+
+  stream.on("finish", function() {
+    // get a blob you can do whatever you like with
+    blob = stream.toBlob("application/pdf")
   })
 }

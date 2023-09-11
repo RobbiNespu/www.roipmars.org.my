88f60b03797e62da3f9570933125f654e1c49c41
Hafizi Ruslan <pizi@hotmail.my>
2023-09-12 00:20:12 +0800

    init bcfy

---
+++ assets/js/bcfy-stat.js
@@ -0,0 +1,5 @@
+document.getElementById('bcfy-status')
+document.getElementById('bcfy-listeners')
+
+const bcfyFeedID = '41368'
+const bcfyOwnAPI = 'https://api.broadcastify.com/owner/?a=feed&feedId='+bcfyFeedID+'&type=json&u=9w2lgx&p=rGwNmcUSLRk2XjTQ'

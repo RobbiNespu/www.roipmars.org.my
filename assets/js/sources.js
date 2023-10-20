dfa906a468f30b94ba9286b99fe921a0401bbeee
Hafizi Ruslan <pizi@hotmail.my>
2023-10-20 11:59:23 +0800

    relocate js to increase loading time

--- assets/js/sources.js
+++ assets/js/sources.js
@@ -38,21 +38,8 @@ const favIcons = '<link rel="apple-touch-icon" sizes="180x180" href="/assets/fav
 <meta name="msapplication-TileImage" content="/assets/favicon/mstile-144x144.png">\
 <meta name="msapplication-config" content="/assets/favicon/browserconfig.xml">\
 <meta name="theme-color" content="#ffffff">'
-const headEndLinks = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css" crossorigin="anonymous">\
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1/font/bootstrap-icons.min.css" crossorigin="anonymous">\
-<link rel="stylesheet" href="https://use.typekit.net/kew7gwq.css">\
-<script src="https://kit.fontawesome.com/221737b641.js" crossorigin="anonymous"></script>\
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-DJYTZHZXFN"></script>\
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1205248207690495" crossorigin="anonymous"></script>\
-<script async src="https://fundingchoicesmessages.google.com/i/pub-1205248207690495?ers=1" nonce="nLBiTr-Hd6zWbl5ZT9wqvA"></script>'
-document.querySelector('head').insertAdjacentHTML('beforeend', referrers + colorScheme + favIcons + headEndLinks)
 
 const bodyStartLinks = '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PHLPNS8" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>\
 <div id="fb-root"></div>\
 <div id="fb-customer-chat" class="fb-customerchat"></div>'
 document.querySelector('body').insertAdjacentHTML('afterbegin', bodyStartLinks)
-
-const bodyEndLinks = '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>\
-<script src="https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js" crossorigin="anonymous"></script>\
-<script src="https://roipmarsnetwork.statuspage.io/embed/script.js" crossorigin="anonymous"></script>'
-document.querySelector('body').insertAdjacentHTML('beforeend', bodyEndLinks)
--- footermin.php
+++ footermin.php
@@ -2,6 +2,13 @@
 <html data-bs-theme='auto'>
 
   <head>
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5/dist/css/bootstrap.min.css' crossorigin='anonymous'>
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1/font/bootstrap-icons.min.css' crossorigin='anonymous'>
+    <link rel='stylesheet' href='https://use.typekit.net/kew7gwq.css'>
+    <script src='https://kit.fontawesome.com/221737b641.js' crossorigin='anonymous'></script>
+    <script async src='https://www.googletagmanager.com/gtag/js?id=G-DJYTZHZXFN'></script>
+    <script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1205248207690495' crossorigin='anonymous'></script>
+    <script async src="https://fundingchoicesmessages.google.com/i/pub-1205248207690495?ers=1" nonce="nLBiTr-Hd6zWbl5ZT9wqvA"></script>
     <script src='/assets/js/gtm.js'></script>
     <script src='/assets/js/gtag.js'></script>
     <script src='/assets/js/gfc.js'></script>
@@ -61,6 +68,9 @@
         </div>
       </div>
     </footer>
+    <script src='https://cdn.jsdelivr.net/npm/bootstrap@5/dist/js/bootstrap.bundle.min.js' crossorigin='anonymous'></script>
+    <script src='https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js' crossorigin='anonymous'></script>
+    <script src='https://roipmarsnetwork.statuspage.io/embed/script.js' crossorigin='anonymous'></script>
     <script src='/assets/js/theme.js'></script>
     <script src='/assets/js/sources.js'></script>
   </body>

37b989eb578304722b98590db27f3d8342e6e2cb
Hafizi Ruslan <pizi@hotmail.my>
2024-01-14 16:59:05 +0800

    add metadata field

--- assets/js/bcfy-stat.js
+++ assets/js/bcfy-stat.js
@@ -8,30 +8,36 @@ setInterval(function () {
     let listeners = data.listeners
     let descr = data.descr
     let bitrate = data.bitrate
-    if (status == 1) {
-      var stats = '<div class="col-4">Status: <span class="text-success-emphasis"><i class="fa-solid fa-volume-high fa-beat-fade"></i> Online</span></div>'
+    let meta = data.meta
+    if (status) {
+      var stats = '<div class="col-auto">Status: <span class="text-success-emphasis"><i class="fa-solid fa-volume-high fa-beat-fade"></i> Dalam Talian</span></div>'
     } else {
-      var stats = '<div class="col-4">Status: <span class="text-danger-emphasis"><i class="fa-solid fa-volume-xmark fa-fade"></i> Offline</span></div>'
+      var stats = '<div class="col-auto">Status: <span class="text-danger-emphasis"><i class="fa-solid fa-volume-xmark fa-fade"></i> Luar Talian</span></div>'
     }
-    if (listeners == 0) {
-      var lists = '<div class="col-4">Pendengar: <span class="text-danger-emphasis">' + listeners + '</span></div>'
+    if (listeners) {
+      var lists = '<div class="col-auto">Pendengar: <span class="text-success-emphasis">' + listeners + '</span></div>'
     } else {
-      var lists = '<div class="col-4">Pendengar: <span class="text-success-emphasis">' + listeners + '</span></div>'
+      var lists = ''
     }
-    if (bitrate == 0) {
-      var rate = '<div class="col-4">Siaran: <span class="text-warning-emphasis">Tidak bersiaran</span></div>'
+    if (bitrate) {
+      var rate = '<div class="col-auto">Siaran: <span class="text-success-emphasis">Sedang berlansung</span></div>'
     } else {
-      var rate = '<div class="col-4">Siaran: <span class="text-success-emphasis">Sedang bersiaran</span></div>'
+      var rate = '<div class="col-auto">Siaran: <span class="text-warning-emphasis">Tidak bersiaran</span></div>'
     }
-    docBcfy.innerHTML = '<div class="col-12"><span>' + descr + '</span></div><div class="row">' + stats + lists + rate + '</div>'
+    if (meta) {
+      var metas = '<br><div class="col-auto">EchoLink Metadata: <span class="text-success-emphasis">' + meta + '</span></div>'
+    } else {
+      var metas = ''
+    }
+    docBcfy.innerHTML = '<div class="col-12"><span>' + descr + '</span></div><div class="row d-flex justify-content-center">' + stats + lists + rate + metas + '</div>'
   })
-}, 10000)
+}, 30000)
 
 $.getJSON(bcfyOwnHook, function (data) {
   let status = data.status
-  if (status == 1) {
+  if (status) {
     docBcfyAud.innerHTML = '<div class="col-12 mt-2"><audio class="rounded-5 w-75 mx-auto" controls preload="none" src="https://broadcastify.cdnstream1.com/41368" type="audio/mp3" crossorigin="anonymous"></audio></div>\
-    <span>Siaran mungkin tertunda sehingga 10 minit bergantung kepada kelajuan dan kepandaman rangkaian.<br>Sebagai alternatif, anda boleh mendengar di sini: <a href="https://stream.mhrtech.my" target="_blank">stream.mhrtech.my</a></span>'
+    <span>Siaran mungkin tertunda sehingga 10 minit bergantung kepada kelajuan dan kependaman rangkaian.<br>Sebagai alternatif, anda boleh mendengar di sini: <a href="https://stream.mhrtech.my" target="_blank">stream.mhrtech.my</a></span>'
   } else {
     docBcfyAud.innerHTML = ''
   }

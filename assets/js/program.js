34719878e8e26d9a29f8f91d3bbee9bcc6bc89e8
Hafizi Ruslan <pizi@hotmail.my>
2024-03-13 02:32:53 +0800

    load kopdarmobile into ecert

--- assets/js/program.js
+++ assets/js/program.js
@@ -745,22 +745,27 @@ $(document).ready(function () {
 					eCert.addFont('/assets/font/SourceSansPro-Regular.ttf', 'SourceSansPro-Regular', 'normal')
 
 					eCertProg.innerText = 'loading images...'
-					// eCert.addImage('/assets/ecert/ecert_template_site.png', 'PNG', 0, 0, 297, 210)
-					await fetch(`/assets/ecert/${source}.jpg`)
+					// eCert.addImage('/media/image/ecert/ecert_template_site.png', 'PNG', 0, 0, 297, 210)
+					await fetch(`/media/image/ecert/${source}.jpg`)
 						.then((response) => {
 							if (response.ok) {
-									eCert.addImage(`/assets/ecert/${source}.jpg`, 'JPEG', 0, 0, 297, 210)
+									eCert.addImage(`/media/image/ecert/${source}.jpg`, 'JPEG', 0, 0, 297, 210)
 							} else {
 								if (activity.toLowerCase().search('sahur') > 0) {
-									eCert.addImage(`/assets/ecert/sahur.jpg`, 'JPEG', 0, 0, 297, 210)
+									eCert.addImage(`/media/image/ecert/sahur.jpg`, 'JPEG', 0, 0, 297, 210)
 								} else {
-									eCert.addImage('/assets/ecert/bg_090324.jpg', 'JPEG', 0, 0, 297, 210)
+									eCert.addImage('/media/image/ecert/bg_090324.jpg', 'JPEG', 0, 0, 297, 210)
 								}
 							}
 						})
-					eCert.addImage('/media/image/brands/roipmars/brand_oglow.png', 'PNG', 98, 10, 100, 20)
-					eCert.addImage('/media/image/roip-concept.png', 'PNG', 10, 179, 62, 20)
-					eCert.addImage('/media/image/malaysian-teamspeak.png', 'PNG', 220, 179, 65, 20)
+					if (activity.toLowerCase().search('sahur') > 0) {
+						eCert.addImage('/media/image/brands/roipmars/brand_oglow.png', 'PNG', 50, 10, 100, 20)
+						eCert.addImage('/media/image/brands/kopdarmobile_sm.png', 'PNG', 160, 10, 100, 20)
+					} else {
+						eCert.addImage('/media/image/brands/roipmars/brand_oglow.png', 'PNG', 98, 10, 100, 20)
+					}
+					eCert.addImage('/media/image/roip-concept.png', 'PNG', 8, 179, 62, 20)
+					eCert.addImage('/media/image/malaysian-teamspeak.png', 'PNG', 225, 179, 65, 20)
 
 					eCertProg.innerText = 'loading texts...'
 					eCert.setFont('Orbitron-Black').setFontSize(30).setTextColor('#72c7ef').text(new Intl.DateTimeFormat('en-MY', { dateStyle: 'long' }).format(new Date(date.split('/')[2], date.split('/')[1] - 1, date.split('/')[0])).toUpperCase(), 148.5, 35, { align: 'center', baseline: 'middle', lineHeightFactor: 1, maxWidth: 280, renderingMode: 'fillThenStroke' })
@@ -808,7 +813,7 @@ $(document).ready(function () {
 					eCert.setFont('KodeMono-Regular').setFontSize(9).setTextColor('black').text(`© ${new Date().getFullYear()} RoIPMARS Network | developed by 9W2LGX | generated via web on ${new Date().toISOString()}`, 148.5, 200, { align: 'center', baseline: 'middle', lineHeightFactor: 1, maxWidth: 280, renderingMode: 'fill' })
 
 					eCert.save(`${date.split('/').reverse().join('-')}_${caller}.pdf`)
-					eCertProg.innerText = `eCert ${date.split('/').reverse().join('-')}_${caller}.pdf saved`
+					eCertProg.innerText = `eCert ${date.split('/').reverse().join('-')}_${caller}.pdf saved. check your 'downloads' folder.`
 				}
 			})
 		})

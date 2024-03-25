4cd0ed4864474bd3299b880bb28d4d52f807790e
Hafizi Ruslan <pizi@hotmail.my>
2024-03-25 23:19:24 +0800

    revamp include path

--- assets/js/sources.js
+++ assets/js/sources.js
@@ -1,9 +1,15 @@
+if (location.port == 80 || location.port == 443) {
+	var currentPort = ''
+} else {
+	var currentPort = ':' + location.port
+}
+let currentHost = location.protocol + '//' + location.hostname + currentPort
 let currentPage = location.pathname.replace('/', '')
 document.querySelector('a#' + currentPage).classList.add('active')
 let currentURL = location.href
 let docTitle = document.querySelector('title').textContent
 let docDesc = document.querySelector('h1').textContent + '\n' + document.querySelector('p').textContent
-const meta = `<base href="${location.hostname}">\
+const meta = `<base href="${currentHost}">\
 <meta charset="utf-8">\
 <meta http-equiv="Content-Type" content="text/html">\
 <meta http-equiv="content-language" content="ms-MY">\
@@ -19,32 +25,33 @@ const meta = `<base href="${location.hostname}">\
 <meta property="og:title" content="RoIPMARS Network | ${docTitle}">\
 <meta property="og:description" content="${docDesc}">\
 <meta property="og:url" content="${currentURL}">\
-<meta property="og:image" content="/assets/icon/favicon-194x194.png">\
+<meta property="og:image" content="${currentHost}/assets/icon/favicon-194x194.png">\
 <meta property="og:type" content="website">\
 <meta property="og:locale" content="ms-MY">\
 <meta name="twitter:title" content="RoIPMARS Network | ${docTitle}">\
 <meta name="twitter:description" content="${docDesc}">\
 <meta name="twitter:url" content="${currentURL}">\
-<meta name="twitter:image" content="/assets/icon/favicon-194x194.png">\
+<meta name="twitter:image" content="${currentHost}/assets/icon/favicon-194x194.png">\
 <meta name="twitter:creator" content="@HafiziRuslan">\
 <meta name="color-scheme" content="auto">`
-const favIcons = '<link rel="apple-touch-icon" sizes="180x180" href="/assets/icon/apple-touch-icon.png">\
-<link rel="icon" type="image/png" sizes="32x32" href="/assets/icon/favicon-32x32.png">\
-<link rel="icon" type="image/png" sizes="194x194" href="/assets/icon/favicon-194x194.png">\
-<link rel="icon" type="image/png" sizes="192x192" href="/assets/icon/android-chrome-192x192.png">\
-<link rel="icon" type="image/png" sizes="16x16" href="/assets/icon/favicon-16x16.png">\
-<link rel="manifest" href="/assets/icon/site.webmanifest">\
-<link rel="mask-icon" href="/assets/icon/safari-pinned-tab.svg" color="#336699">\
-<link rel="shortcut icon" href="/assets/icon/favicon.ico">\
+const favIcons = `<link rel="apple-touch-icon" sizes="180x180" href="${currentHost}/assets/icon/apple-touch-icon.png">\
+<link rel="icon" type="image/png" sizes="32x32" href="${currentHost}/assets/icon/favicon-32x32.png">\
+<link rel="icon" type="image/png" sizes="194x194" href="${currentHost}/assets/icon/favicon-194x194.png">\
+<link rel="icon" type="image/png" sizes="192x192" href="${currentHost}/assets/icon/android-chrome-192x192.png">\
+<link rel="icon" type="image/png" sizes="16x16" href="${currentHost}/assets/icon/favicon-16x16.png">\
+<link rel="manifest" href="${currentHost}/assets/icon/site.webmanifest">\
+<link rel="mask-icon" href="${currentHost}/assets/icon/safari-pinned-tab.svg" color="#336699">\
+<link rel="shortcut icon" href="${currentHost}/assets/icon/favicon.ico">\
 <meta name="apple-mobile-web-app-title" content="RoIPMARS">\
 <meta name="application-name" content="RoIPMARS">\
 <meta name="msapplication-TileColor" content="#2d89ef">\
-<meta name="msapplication-TileImage" content="/assets/icon/mstile-144x144.png">\
-<meta name="msapplication-config" content="/assets/icon/browserconfig.xml">\
-<meta name="theme-color" content="#ffffff">'
+<meta name="msapplication-TileImage" content="${currentHost}/assets/icon/mstile-144x144.png">\
+<meta name="msapplication-config" content="${currentHost}/assets/icon/browserconfig.xml">\
+<meta name="theme-color" content="#ffffff">`
 document.querySelector('head').insertAdjacentHTML('afterbegin', meta + favIcons)
 
-const bodyStartLinks = '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PHLPNS8" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>\
+const bodyStartLinks =
+	'<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PHLPNS8" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>\
 <div id="fb-root"></div>\
 <div id="fb-customer-chat" class="fb-customerchat"></div>\
 <script>var chatbox = document.getElementById("fb-customer-chat"); chatbox.setAttribute("page_id", "100870702762488"); chatbox.setAttribute("attribution", "biz_inbox");</script>\
--- nts/2022/02.php
+++ nts/2022/02.php
@@ -6,7 +6,7 @@
   </head>
 
   <body>
-    <?php include '../../ntsheader.php' ?>
+    <?php include './ntsheader.php' ?>
     <div class='container my-2'>
       <p class='fs-1 text-center'>Perubahan Platform Kedai RoIPMARS</p>
       <table class='table table-sm table-borderless fs-5 text-center d-flex justify-content-end'>
@@ -25,7 +25,7 @@
       <p class='fs-4 text-end mt-2 mb-0 ft-signature'>Pentadbir Rangkaian</p>
       <p class='text-end mt-0 mb-2' style='font-family: ui-monospace, "Cascadia Code", "Source Code Pro", Menlo, Consolas, "DejaVu Sans Mono", monospace; font-weight: 500;'>Pengurusan Rangkaian Kumpulan RoIPMARS</p>
     </div>
-    <?php include '../../footermin.php' ?>
+    <?php include './footermin.php' ?>
   </body>
 
 </html>

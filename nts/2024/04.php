d5c2ec821d90c69445bd2f0df7077a66cf11c3cb
Hafizi Ruslan <pizi@hotmail.my>
2024-04-18 07:16:24 +0800

    rename title to peralihan

--- nts/2024/04.php
+++ nts/2024/04.php
@@ -2,13 +2,13 @@
 <html lang='ms-MY' data-bs-theme='auto'>
 
   <head>
-    <title>RoIPMARS Network | Notis Naik Taraf Layanan Fail Sijil &amp; eQSL siap</title>
+    <title>RoIPMARS Network | Notis Peralihan Koleksi Sijil &amp; eQSL siap</title>
   </head>
 
   <body>
     <?php include __DIR__.'/../../ntsheader.php' ?>
     <div class='container my-2'>
-      <h1 class='text-center'>Naik Taraf Layanan Fail Sijil &amp; eQSL siap</h1>
+      <h1 class='text-center'>Peralihan Koleksi Sijil &amp; eQSL siap</h1>
       <table class='table table-sm table-borderless fs-5 text-center d-flex justify-content-end'>
         <tbody>
           <tr><td>Rujukan Notis</td><td>NtsMARS2024/04</td></tr>
@@ -16,8 +16,8 @@
         </tbody>
       </table>
       <p class='lh-sm'>Pengguna yang dihormati,</p>
-      <p class='fs-2 fw-bold text-decoration-underline'>Per: Notis Naik Taraf Layanan Fail Sijil &amp; eQSL siap</p>
-      <p>Merujuk kepada perkara diatas, adalah dimaklumkan bahawa fail sijil &amp; eQSL siap akan dialihkan bagi memberi ruang kepada peruntukan sumber pelayan kami untuk jangka panjang.</p>
+      <p class='fs-2 fw-bold text-decoration-underline'>Per: Notis Peralihan Koleksi Sijil &amp; eQSL siap</p>
+      <p>Merujuk kepada perkara diatas, adalah dimaklumkan bahawa koleksi sijil &amp; eQSL akan dialihkan bagi memberi ruang kepada peruntukan sumber pelayan kami untuk jangka panjang.</p>
       <table class='table table-sm align-middle'>
         <tbody>
           <tr>
@@ -31,15 +31,15 @@
           <tr>
             <th>Halaman sumber</th>
             <td><ul>
-              <li><a href='./program' target='_blank'>Jadual Aktiviti &amp; Program</a></li>
+              <li><a href='./program' target='_blank'>Aktiviti &amp; Program</a></li>
               <li><a href='./cbmars' target='_blank'>Jalur Rakyat RoIPMARS</a></li>
               <li><a href='./member' target='_blank'>Keahlian RoIPMARS</a></li>
             </ul></td>
           </tr>
         </tbody>
       </table>
-      <p>Buat masa ini, semua fail sijil &amp; eQSL siap masih boleh didapati di <a href='https://dl.roipmars.org.my/#files%2Fcertificates' target='_blank'>dl.roipmars.org.my</a> sehingga tarikh diatas. Selepas pada tarikh diatas, segala fail sijil &amp; eQSL akan dialihkan ke pelayan pihak ketiga iaitu <a href='https://archived.mhrtech.my' target='_blank'>archived.mhrtech.my</a>. Dalam masa yang sama, semua eQSL yang dihantar ke bahagian rekod tidak lagi wajib seperti sebelum ini, pembuatan &amp; pengedarannya adalah tanggungjawab NCS sendiri dan pihak ketiga &lpar;seperti diatas&rpar; tidak memberi jaminan bahawa semua fail yang dihantar akan dimuatnaik ke pelayan tersebut.</p>
-      <p>Namun yang demikian, anda boleh mula menggunakan penjana sijil yang telah diskripkan pada halaman sumber seperti diatas dengan memilih dan klik pada baris data pilihan anda.</p>
+      <p>Buat masa ini, semua koleksi sijil &amp; eQSL siap masih boleh didapati di <a href='https://dl.roipmars.org.my/#files%2Fcertificates' target='_blank'>dl.roipmars.org.my</a> sehingga tarikh diatas. Selepas pada tarikh diatas, segala koleksi sijil &amp; eQSL akan dialihkan ke pelayan pihak ketiga iaitu <a href='https://archived.mhrtech.my' target='_blank'>archived.mhrtech.my</a>. Dalam masa yang sama, semua eQSL yang dihantar ke bahagian rekod tidak lagi wajib seperti sebelum ini, pembuatan &amp; pengedarannya adalah tanggungjawab NCS sendiri dan pihak ketiga tidak memberi jaminan bahawa semua koleksi yang dihantar akan dimuatnaik ke pelayan tersebut.</p>
+      <p>Namun yang demikian, anda boleh mula menggunakan penjana sijil pada halaman sumber seperti diatas dengan memilih dan klik pada tanda panggilan anda.</p>
       <p>Pihak Pengurusan Rangkaian Kumpulan RoIPMARS memohon maaf atas kesulitan yang bakal dihadapi demi seluruh kelansungan Rangkaian Kumpulan RoIPMARS.</p>
       <p class='text-center'>Sekiranya anda memerlukan bantuan, sila emel kepada: <a href='mailto:member@roipmars.org.my?subject=Inkuiri%20Notis2404'>member@roipmars.org.my</a>
       <p class='fs-4 text-end mt-2 mb-0 ft-signature'>Pentadbir Rangkaian</p>

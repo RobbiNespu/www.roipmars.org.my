61fac87d8714396b854ede4928596cebaa9f265c
Hafizi Ruslan <pizi@hotmail.my>
2024-06-22 05:16:02 +0800

    upgrade fb.init

--- assets/js/sources.js
+++ assets/js/sources.js
@@ -75,7 +75,7 @@ const bodyStartLinks = `<noscript><iframe src="https://www.googletagmanager.com/
 <div id="fb-root"></div>\
 <div id="fb-customer-chat" class="fb-customerchat"></div>\
 <script>var chatbox = document.getElementById("fb-customer-chat"); chatbox.setAttribute("page_id", "100870702762488"); chatbox.setAttribute("attribution", "biz_inbox");</script>\
-<script>window.fbAsyncInit = function() {FB.init({xfbml: true,version: "v18.0"}); }; (function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id;js.src = "https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js"; fjs.parentNode.insertBefore(js, fjs); }(document, "script", "facebook-jssdk"));</script>`
+<script>window.fbAsyncInit = function() {FB.init({xfbml: true,version: "v20.0"}); }; (function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id;js.src = "https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js"; fjs.parentNode.insertBefore(js, fjs); }(document, "script", "facebook-jssdk"));</script>`
 document.body.insertAdjacentHTML('afterbegin', bodyStartLinks)
 
 function dtCFormat(input) {
